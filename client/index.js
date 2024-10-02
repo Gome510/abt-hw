21eea75bb4d35de75f76bdae2ce0b3bceb37b4eb
Pablo Gomez-Echegaray <pgomezec@ucsc.edu>
2024-10-02 14:06:39 -0700

    checkpoint-10

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import { onRegisterClick } from "./src/onRegisterClick";
 import { updateLotteries } from "./src/updateLotteries";
 
+const POLLING_INTERVAL_IN_MS = 10000;
+
 const registerButton = document.getElementById("register");
 registerButton.onclick = onRegisterClick;
 
 updateLotteries();
+
+setInterval(()=> updateLotteries(), POLLING_INTERVAL_IN_MS);
