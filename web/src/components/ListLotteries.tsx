1874c9ccd99e8914be322432292d840aa4ebbdd0
Gome510 <bluehawkfire55@gmail.com>
2024-11-02 12:04:51 -0700

    fetchlotteries

--- web/src/components/ListLotteries.tsx
+++ web/src/components/ListLotteries.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+const baseUrl = 'http://localhost:3000';
 function ListLotteries() {
+  const [lotteries, setLotteries] = useState([]);
+
+  async function fetchLotteries() {
+    const response = await fetch(`${baseUrl}/lotteries`);
+    const data = await response.json();
+    setLotteries(data);
+  }
+  useEffect(() => {
+    fetchLotteries();
+  }, []);
+
   return <div>ListLotteries</div>;
 }
 
