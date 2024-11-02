43e57f8fd18eaa1214d8aa5aa3f02f913309dfba
Pablo Gomez-Echegaray <pgomezec@ucsc.edu>
2024-11-02 16:49:19 -0700

    display lottery list and styling layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css';
+import { Box, Container, Typography } from '@mui/material';
+import CasinoIcon from '@mui/icons-material/Casino';
 import AddLottery from './components/AddLottery';
+import ListLotteries from './components/ListLotteries';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <AddLottery />
-    </div>
+    <Container>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h1">
+          Lotteries{' '}
+          <CasinoIcon fontSize="inherit" sx={{ verticalAlign: 'middle' }} />
+        </Typography>
+        <ListLotteries />
+        <AddLottery />
+      </Box>
+    </Container>
   );
 }
 
--- web/src/components/ListLotteries.tsx
+++ web/src/components/ListLotteries.tsx
@@ -1,21 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import SyncIcon from '@mui/icons-material/Sync';
+import { type Lottery } from '../../../backend/types';
+import { Box, Button, IconButton, Typography } from '@mui/material';
 
 const baseUrl = 'http://localhost:3000';
 function ListLotteries() {
-  const [lotteries, setLotteries] = useState([]);
+  const [lotteries, setLotteries] = useState<Lottery[]>([]);
 
-  async function fetchLotteries() {
-    const response = await fetch(`${baseUrl}/lotteries`);
-    const data = await response.json();
-    setLotteries(data);
-  }
   useEffect(() => {
-    fetchLotteries();
+    async function fetchLotteries() {
+      try {
+        const response = await fetch(`${baseUrl}/lotteries`);
+        const data = (await response.json()) as Lottery[];
+        setLotteries(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    void fetchLotteries();
   }, []);
 
-  return <div>ListLotteries</div>;
+  return (
+    //TODO: Responsive column count
+    <Box maxWidth={900}>
+      {lotteries
+        ? lotteries.map((lottery) => <LotteryCard lottery={lottery} />)
+        : 'ListLotteries'}
+    </Box>
+  );
 }
 
 export default ListLotteries;
 
-function LotteryCard() {}
+interface LotteryCardProps {
+  lottery: Lottery;
+}
+function LotteryCard({ lottery }: LotteryCardProps) {
+  return (
+    <Card sx={{ width: 400 }}>
+      <CardContent sx={{ position: 'relative' }}>
+        <IconButton
+          color="inherit"
+          sx={{ position: 'absolute', right: '1rem' }}
+        >
+          <SyncIcon />
+        </IconButton>
+        <Typography variant="h5" align="left">
+          {lottery.name}
+        </Typography>
+        <Typography align="left"> {lottery.prize} </Typography>
+        <Typography align="left"> {lottery.id} </Typography>
+      </CardContent>
+    </Card>
+  );
+}
